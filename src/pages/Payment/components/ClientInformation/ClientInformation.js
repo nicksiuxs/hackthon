d53e68d6ce646ef6f91550956921c1e31a67bdf2
David Lopez <david.lopez@veevart.com>
2022-08-06 12:25:00 -0500

    do payment for rental event

--- src/pages/Payment/components/ClientInformation/ClientInformation.js
+++ src/pages/Payment/components/ClientInformation/ClientInformation.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import useAppContext from '../../../../customHooks/useAppContext';
+import usePayRental from '../../../../customHooks/usePayRental';
 import CardInformation from './CardInformation/CardInformation';
 import PersonalInformation from './PersonalInformation/PersonalInformation';
 
 const ClientInformation = () => {
 
   const { state: { summary } } = useAppContext();
+  const { doPayment } = usePayRental();
+
   return (
     <div className='panel panel-70'>
       <PersonalInformation />
       <CardInformation />
-      <button className='btn'>Pay ${summary.total}</button>
+      <button
+        className='btn'
+        onClick={() => doPayment()}
+      >
+        Pay ${summary.total}
+      </button>
     </div>
   )
 }
